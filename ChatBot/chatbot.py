0a053dd6ee32c281b2779ed427baaf0a01c9f170
AlekseySirobaba <o.v.sirobaba@student.khai.edu>
2021-09-24 18:49:17 +0300

    ChatBot 4-th stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -8,3 +8,7 @@ remainder5 = int(input())
 remainder7 = int(input())
 age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 print("Your age is " + str(age) + "; that`s a good time to start programming!")
+print("Now I will prove to you that I can count any number you want.")
+counting_number = int(input())
+for a in range(1, counting_number + 1):
+    print(str(a) + " !")
