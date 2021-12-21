7244d38dbd907226ed3c89eaf1899cb2d8fbfd2c
Aleksey <o.v.sirobaba@student.khai.edu>
2021-12-21 10:38:27 +0200

    Dominoes v.1.0

---
+++ dominoes/Stage_1.py
@@ -0,0 +1,101 @@
+import random
+
+
+class Domino:
+    def __init__(self):
+        self.dominoes = [[0, 0],
+                         [0, 1], [1, 1],
+                         [0, 2], [1, 2], [2, 2],
+                         [0, 3], [1, 3], [2, 3], [3, 3],
+                         [0, 4], [1, 4], [2, 4], [3, 4], [4, 4],
+                         [0, 5], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5],
+                         [0, 6], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6], [6, 6]
+                         ]
+        self.player = []
+        self.computer = []import random
+
+
+class Domino:
+    def __init__(self):
+        self.dominoes = [[0, 0],
+                         [0, 1], [1, 1],
+                         [0, 2], [1, 2], [2, 2],
+                         [0, 3], [1, 3], [2, 3], [3, 3],
+                         [0, 4], [1, 4], [2, 4], [3, 4], [4, 4],
+                         [0, 5], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5],
+                         [0, 6], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6], [6, 6]
+                         ]
+        self.player = []
+        self.computer = []
+        self.stock = []
+        self.snake = []
+        self.status = ""
+
+    # Creates a set of dominoes to computer and player
+    def creating(self):
+        # Creates the main massive of all 28 dominoes
+        dominoes = self.dominoes.copy()
+
+        # Gives 7 random dominoes to computer and player and removes these dominoes from main massive
+        for piece in range(7):
+            self.player.append(random.choice(dominoes))
+            dominoes.remove(self.player[piece])
+            self.computer.append(random.choice(dominoes))
+            dominoes.remove(self.computer[piece])
+        self.stock = dominoes.copy()
+        game.game_conditions()
+
+    # Checks if all conditions to play game are satisfied
+    def game_conditions(self):
+
+        # Finds in players sets first domino with same numbers to place and who`s first
+        for i in range(7):
+            if self.player[i][0] == self.player[i][1]:
+                self.snake.append(self.player[i])
+                self.player.remove(self.player[i])
+                self.status = "computer"
+                game.output()
+                break
+
+            elif self.computer[i][0] == self.computer[i][1]:
+                self.snake.append(self.computer[i])
+                self.computer.remove(self.computer[i])
+                self.status = "player"
+                game.output()
+                break
+
+            # If there are no equal dominoes the algorithm
+            else:
+                game.__init__()
+                game.creating()
+                break
+
+    # Outputs the result
+    def output(self):
+        print(f"""Stock pieces: {self.stock}
+        Computer pieces: {self.computer}
+        Player pieces: {self.player}
+        Domino snake: {self.snake}
+        Status: {self.status}""")
+        print()
+        menu()
+
+
+# Game menu
+def menu():
+    game.__init__()
+    action = input("""1. Play
+0. Exit\n""")
+
+    if action == '1':
+        game.creating()
+    elif action == '0':
+        pass
+    else:
+        menu()
+
+
+# Starts the programme
+if __name__ == "__main__":
+    game = Domino()
+    menu()
